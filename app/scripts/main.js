d5657e9cecd5d13fb1f4165758ff1829b94c48ef
61quisberth <61quisberth@cardinalmail.cua.edu>
2015-02-21 13:52:02 -0500

    attempted to add map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,7 +22,9 @@
     window.app = app;
 	map = {
 		initialize: function(){
-
+	L.mapbox.accessToken = 'pk.eyJ1IjoiNjFxdWlzYmVydGgiLCJhIjoib1lsUTNsbyJ9.UZh_yfTjTscW-48eFaWCQQ';
+	map = L.mapbox.map('map', 'examples.map-i86nkdio')
+    .setView([40, -74.50], 9);
 		} 
 	};
 
